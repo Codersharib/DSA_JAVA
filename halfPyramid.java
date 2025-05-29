fe07737288212e5fb1cd4a40cc9c0ea7fd11e6de
codersharib <codersharib@gmail.com>
2025-05-29 12:27:46 +0530

    All pattern questions.

--- halfPyramid.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class halfPyramid{
-    public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    System.out.println("enter no of line of pattern  : ");
-    int n=sc.nextInt();
-    
-    }
-}
